a08a437eff6142df2b426660bf1b5534b8b5556e
jeonjihyeong <102641699+jeonjihyeong@users.noreply.github.com>
2022-07-08 20:15:20 +0900

    회원가입 중복 회원가입 제거 기능

--- second/server/index.js
+++ second/server/index.js
@@ -3,32 +3,59 @@ const app = express()
 const port = 3000
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const db= require('./lib/db')
+const service=require('./service')
+
+
+
 app.use(bodyParser.json())
 app.use(cors());
 
-const db = {
-  id : 'aaa',
-  pw : 'aaa'
-} 
-
-app.get('/id/:id',(req,res)=>{
-  console.log(req.params)
-  res.send(db)
+// app.get('/ping',async(req,res)=>{
+//   const ress = await service.getUserId();
   
-})
+//   console.log(ress.id)
 
-app.post('/login', (req, res) => {
-  if(req.body.id === db.id){
-    if(req.body.pw === db.pw){
-      res.send('success');
-    }else {
-      res.send('failpw');
-    }
-  }else {
-    res.send('failid');
+// })
+
+app.post('/signup',async(req,res)=>{
+  const data= req.body;
+  const duplicateCheck= await service.getUserId(data.id);
+  console.log(duplicateCheck)
+  if (duplicateCheck!==null){
+    console.log('id가 이미 존재합니다.');
+    res.send({data: 0})
+  }else{
+    const result = await service.saveUser(data.id,data.pw,data.age,data.name);
+    console.log(result);
+    res.send({data: 1})
   }
 })
 
-app.listen(port, () => {
+// app.get('/id/:id',(req,res)=>{
+//   console.log(req.params)
+//   res.send(db)
+  
+// })
+
+
+
+app.post('/login', async(req, res) => {
+  const data=req.body;
+  const idData=await service.getUserId(data.id);
+  if (idData===null){
+    res.send ({data: 'not exist'})
+  }else {
+      const pwData=idData.dataValues.pw
+      if(data.pw!==pwData){
+          res.send({data: 'wrong password'})
+      }else {
+          res.send({data: 1});
+      }
+  }
+})
+
+app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`)
+  await db.initialize()
 })
