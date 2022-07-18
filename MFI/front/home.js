02063637513d3c70c611759c5d26764a88b23f03
jeonjihyeong <102641699+jeonjihyeong@users.noreply.github.com>
2022-07-18 17:40:56 +0900

    로그인/로그아웃 버튼 태그 변경 레이어 분할

--- MFI/front/home.js
+++ MFI/front/home.js
@@ -1,1 +1,15 @@
-const 
+window.onload =()=>{
+    const token = localStorage.getItem('accessToken')
+    if(token){
+        loginSucces();
+    }
+}
+const loginSucces = ()=>{
+    const loginButton=document.getElementById('login')
+    loginButton.innerHTML='로그아웃';
+    loginButton.onclick=logout();
+}
+
+const logout=()=>{
+    localStorage.removeItem('accessToken');
+}
