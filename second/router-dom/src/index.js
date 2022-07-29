2197003ca2440056f1cd9989c897928813196606
jeonjihyeong <spdlqj7014@naver.com>
2022-07-29 20:20:50 +0900

    프론트 레이어 리팩토링

--- second/router-dom/src/index.js
+++ second/router-dom/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './views/index.css';
+import App from './component/App';
 import reportWebVitals from './reportWebVitals';
 
 
