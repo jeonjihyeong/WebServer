0446693301c990e188a69dc0d18ff2207c743708
jeonjihyeong <102641699+jeonjihyeong@users.noreply.github.com>
2022-08-30 17:48:23 +0900

    vue 프로젝트 수정

---
+++ MFI/vue-front/vue-project/src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+	<h1>Hello, Vue!</h1>
+</template>
+
+<script>
+export default {
+	name: 'testComponent'
+}
+</script>
+
+<style>
+	h1 {
+		color: #42b983;
+	}
+</style>
---
+++ WebDB/hello.js
@@ -0,0 +1,6 @@
+console.log("안녕하세요")
+console.log("안녕하세요")
+console.log("안녕하세요")
+console.log("안녕하세요")
+console.log("안녕하세요")
+console.log("안녕하세요")
